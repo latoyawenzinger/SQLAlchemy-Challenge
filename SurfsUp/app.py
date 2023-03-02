942738d35f966d3fd099ac1e7ad7f4046d54f223
Latoya Wenzinger <lnorman1016@gmail.com>
2023-03-02 16:06:11 -0500

    updated start/end routes

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,4 +1,5 @@
 import numpy as np
+import datetime as dt
 
 # import Flask
 from flask import Flask, jsonify
@@ -19,7 +20,7 @@ Base = automap_base()
 Base.prepare(engine, reflect=True)
 
 #assign each class to a variable
-Measurement = Base.classes.measurement
+measurement = Base.classes.measurement
 Station = Base.classes.station
 
 
@@ -32,18 +33,21 @@ app = Flask(__name__)
 def home():
     return (
         f"Available Routes:<br/>"
-        f"/api/v1.0/precipitation<br/>"
-        f"/api/v1.0/stations<br/>"
-        f"/api/v1.0/tobs<br/>")
+        f"<strong>/api/v1.0/precipitation</strong><br/>"
+        f"<strong>/api/v1.0/stations</strong><br/>"
+        f"<strong>/api/v1.0/tobs</strong><br/>"
+        f"<strong>/api/v1.0/start</strong>  (REPLACE START WITH ACTUAL DATE - MUST FORMAT: yyyy-mm-dd) <br/>"
+        f"<strong>/api/v1.0/start/end</strong>  (REPLACE START WITH ACTUAL DATE - MUST FORMAT: yyyy-mm-dd/yyyy-mm-dd)")
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
+    """Return precipitation data for the last 12 months as json"""
     #start session
     session = Session(engine)
 
     # query precipitation results from the last 12 months
-    precipitation = session.query(Measurement.date, Measurement.prcp).\
-        filter(Measurement.date.between('2016-08-23', '2017-08-23')).all()
+    precipitation = session.query(measurement.date, measurement.prcp).\
+        filter(measurement.date.between('2016-08-23', '2017-08-23')).all()
 
     session.close()
 
@@ -60,6 +64,7 @@ def precipitation():
 
 @app.route("/api/v1.0/stations")
 def stations():
+    """Return a JSON list of stations from the dataset"""
     #start session
     session = Session(engine)
 
@@ -83,12 +88,13 @@ def stations():
 
 @app.route("/api/v1.0/tobs")
 def tobs():
+    """Return a JSON list of temperatures observations for the previous year using the most-active station"""
     # start session
     session = Session(engine)
 
     # query the dates and temperature observations of the most-active station for the previous year of data
-    temperature = session.query(Measurement.date, Measurement.tobs).filter(Measurement.station == 'USC00519281').\
-        filter(Measurement.date.between('2016-08-23', '2017-08-23')).all()
+    temperature = session.query(measurement.date, measurement.tobs).filter(measurement.station == 'USC00519281').\
+        filter(measurement.date.between('2016-08-23', '2017-08-23')).all()
 
     session.close()
 
@@ -100,8 +106,49 @@ def tobs():
 
     return jsonify(all_temps)
 
+@app.route("/api/v1.0/<start>")
+@app.route("/api/v1.0/<start>/<end>")
+def date(start, end=None):
+
+    session = Session(engine)
+    # if start and end date is given
+
+    start = dt.datetime.strptime(start, '%Y-%m-%d')
+    end = dt.datetime.strptime(end, '%Y-%m-%d')
+
+    
+    if not end: 
+        start_date = session.query(func.min(measurement.tobs),func.max(measurement.tobs),func.avg(measurement.tobs)).\
+        filter(measurement.date.between(start, end)).all()
+        
+
+    #if no end date is given
+    else:
+
+        start_date = session.query(func.min(measurement.tobs),func.max(measurement.tobs),func.avg(measurement.tobs)).\
+        filter(measurement.date >= start).all()
+        
+        
+        #filter(measurement.date.between(start, end)).all()
+
+    
+    session.close()
+    
+
+    date_list = []
+    for min, max, avg in start_date:
+        date_dict = {}
+        date_dict['min_temp'] = min
+        date_dict['max_temp'] = max
+        date_dict['avg_temp'] = avg
+
+        date_list.append(date_dict)
+
+        return jsonify(date_list)
+    
 
 
+        
 if __name__ == '__main__':
     app.run(debug=True)
 
